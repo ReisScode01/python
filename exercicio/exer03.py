4add926f6fc1540c6b3f1c24c659dfe01475f8b0
ReisScode01 <161728482+ReisScode01@users.noreply.github.com>
2024-04-17 11:27:03 -0300

    Create exer03.py

---
+++ exercicio/exer03.py
@@ -0,0 +1,6 @@
+nota1 = float(input("A nota do primeiro trimestre"))
+nota2 = float(input("A nota do segundo trimestre"))
+nota3 = float(input("A nota do terceiro trimestre"))
+nota4 = float(input("A nota do quarto trimestre"))
+media = (nota1+nota2+nota3+nota4)/4
+print("a média é:",media) 
