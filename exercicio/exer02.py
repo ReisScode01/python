9e362de20a9102084459f6621f950f9d6eabf44c
ReisScode01 <161728482+ReisScode01@users.noreply.github.com>
2024-04-17 11:26:26 -0300

    Create exer02.py

---
+++ exercicio/exer02.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um número menor que 100:"))
+if numero>=100:
+        print("O número digitando é maior que 100:")
+else:
+        dezena=numero//10
+        unidade=numero%10
+soma_digitos=dezena+unidade
+    
+print("A soma dos dígitos de ", numero, "é igual a ", soma_digitos)
